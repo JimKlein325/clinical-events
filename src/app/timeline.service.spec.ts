d4c8357b40851c6017d0973d81a6f9147035808c
Jim Klein <j.mesklein@gmail.com>
2017-09-10 20:49:44 -0700

    update timelineservice.prepareData tests.

--- src/app/timeline.service.spec.ts
+++ src/app/timeline.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { TestData } from "./model/test-data";
 import { TimelineService } from './timeline.service';
 
 describe('TimelineService', () => {
@@ -10,6 +10,24 @@ describe('TimelineService', () => {
   });
 
   it('should be created', inject([TimelineService], (service: TimelineService) => {
+    const preparedData = service.prepareData(TestData.dataset);
     expect(service).toBeTruthy();
   }));
+  describe('prepareData()', () => {
+    it('should place Diagnosis information above axis', inject([TimelineService], (service: TimelineService) => {
+      const prepareData = service.prepareData(TestData.dataset);
+      // console.log(prepareData[0].yValue);
+      expect(prepareData[0].yValue).toBeGreaterThan(0);
+    }));
+    it('should place Treatment information above axis', inject([TimelineService], (service: TimelineService) => {
+      const prepareData = service.prepareData(TestData.dataset);
+      // console.log(prepareData[2].yValue);
+      expect(prepareData[2].yValue).toBeGreaterThan(0);
+    }));
+    it('should place Quality of Life information above axis', inject([TimelineService], (service: TimelineService) => {
+      const prepareData = service.prepareData(TestData.dataset);
+      // console.log(prepareData[prepareData.length -1].yValue);
+      expect(prepareData[prepareData.length -1].yValue).toBeLessThan(0);
+    }));
+  })
 });
